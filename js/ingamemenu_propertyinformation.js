7c677974318196e9bb4a47684a9da11347434238
Vince McKeown <vmckeown79@gmail.com>
2020-07-09 21:48:37 -0400

    removed console.logs

--- js/ingamemenu_propertyinformation.js
+++ js/ingamemenu_propertyinformation.js
@@ -1,18 +1,18 @@
 function inGameMenu_PropertyInformation_Definition(){
 	this.init = function(){
-		console.log("opening info for property information");
+		//console.log("opening info for property information");
 	}
 	this.exit = function(){
-		console.log("exiting info for property information");
+		//console.log("exiting info for property information");
 	}
 	this.update = function(){
-		console.log("updating info for property information");
+		//console.log("updating info for property information");
 	}
 	this.draw = function(){
-		console.log("draw info for property information");
+		//console.log("draw info for property information");
 	}
 	this.handleKey = function(keyCode){
-		console.log(keyCode + " handleKey  info for property information");
+		//console.log(keyCode + " handleKey  info for property information");
 		changeState(null);
 	}
 }
