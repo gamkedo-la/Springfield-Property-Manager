41b002eeeb3f09c3834efe5c321ed7d7708e4887
Vince McKeown <vmckeown79@gmail.com>
2020-07-12 22:57:14 -0400

    WIP:  inGameMenu_PropertyInformation Screen

--- js/ingamemenu_propertyinformation.js
+++ js/ingamemenu_propertyinformation.js
@@ -1,4 +1,9 @@
 function inGameMenu_PropertyInformation_Definition(){
+	this.propertyInformationTextXPos = 20;
+	this.propertyInformationTextYPos = 80;
+	this.textLineSpacing = 30;
+	this.propertyInformation = ["Lot Number: ", "Property Purchase Price: ", "Zoned: ", "Building Type: "]
+	
 	this.init = function(){
 		//console.log("opening info for property information");
 	}
@@ -9,7 +14,20 @@ function inGameMenu_PropertyInformation_Definition(){
 		//console.log("updating info for property information");
 	}
 	this.draw = function(){
-		//console.log("draw info for property information");
+		//Setting up the canvas for sections
+		colorRect(0,0,canvas.width,canvas.height, "green");
+		colorRect(10,10,810,30, "white"); //Menu Title
+		colorRect(10,50,400,260, "white"); //Property Information Space
+		colorRect(10,320,810, 300, "white"); //Space to put future graph
+		colorRect(420,50,400,260, "white");  // Property Picture Space
+		//Text for Menu Title
+		colorText("Property Information", 280, 33, "black", font = "26px Arial Black");
+		//Property Picture
+		drawBitmapAtLocation(buildingPic, 570, 100);
+		//Text Data for Property Information
+		for(i = 0; i < this.propertyInformation.length; i++){
+			colorText(this.propertyInformation[i], this.propertyInformationTextXPos, this.propertyInformationTextYPos + (this.textLineSpacing * [i]), "black", font = "14px Arial Black");
+		}		
 	}
 	this.handleKey = function(keyCode){
 		//console.log(keyCode + " handleKey  info for property information");
