cdadba01fe53478ce14dc4c59e89a152a377e8bc
Gabriel Cornish <58050002+gabrielcornish@users.noreply.github.com>
2020-08-05 08:18:58 -0700

    Fixed Month and Day String overlap

--- js/gameTime.js
+++ js/gameTime.js
@@ -32,7 +32,7 @@ function updateTime(){
 function displayGameTime(){
 	//colorText("X: " + mousePosX + " Y: " + mousePosY, 15, 10, "#fafdff", "14px 'b612regular'");
 	colorText(gameMonth[whichMonth] + " " + gameYear, 800,  900, "#ffd100", "30px 'lexendpeta'");
-	colorText("Day: " + gameDay, 1000,  900, "#ffd100", "30px 'lexendpeta'");
+	colorText("- Day: " + gameDay, 1100,  900, "#ffd100", "30px 'lexendpeta'");
 }
 
 function changeMonth(){
