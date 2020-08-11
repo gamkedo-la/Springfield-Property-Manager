cc97dc808ac6f4010dfce7a7b2d170df44f855d0
Gonzalo Delgado <mail@gonzalodelgado.com>
2020-08-11 07:57:34 -0300

    Improve randomIntFromInverval slightly.

--- js/math.js
+++ js/math.js
@@ -1,4 +1,6 @@
 function randomIntFromInterval(min,max) // min and max included
 {
-    return Math.floor(Math.random()*(max-min+1)+min);
+	min = Math.ceil(min);
+	max = Math.floor(max);
+    return Math.floor(Math.random()*(max-min+1)) + min;
 }
