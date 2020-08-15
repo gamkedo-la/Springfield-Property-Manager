6f043de25d33e561add45b2736b5cad8bfec3ca1
Vince McKeown <vmckeown79@gmail.com>
2020-08-14 21:15:22 -0400

    Refactored weather code for storm clouds

--- js/weather.js
+++ js/weather.js
@@ -14,6 +14,10 @@ function weatherClass() {
 		this.x = randomIntFromInterval(-600,canvas.width);
 		this.y = randomIntFromInterval(0,canvas.height - 400);
 		this.vel = Math.random();
+		this.checkForStormCloud();
+	}
+	
+	this.checkForStormCloud = function(){
 		var randomCloudPic = randomIntFromInterval(1,100)
 		if(randomCloudPic <= 50){
 			this.pic = cloud1Pic;
@@ -22,22 +26,7 @@ function weatherClass() {
 		} else {
 			this.stormCloud = true;
 			this.pic = cloud3Pic;
-		}
-	}
-	
-	this.checkForStormCloud = function(){
-		var chanceOfStormCloud = randomIntFromInterval(1,10);
-		if(chanceOfStormCloud <= 5){
-			this.stormCloud = true;
-			this.pic = cloud3Pic;
-		} else if (chanceOfStormCloud > 6 && chanceOfStormCloud < 8) {
-			this.stormCloud = false;
-			this.pic = cloud1Pic;
-		} else {
-			this.stormCloud = false;
-			this.pic = cloud2Pic;
-		}
-		
+		}	
 	}
 	
 	this.move = function(){
